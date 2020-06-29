121c8edf90b9c6171d9400323732218aa992c947
sqlkrad <56990007+sqlkrad@users.noreply.github.com>
2020-06-29 11:47:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='magic_google',
-    version='0.2.8',
+    version='0.2.9',
     description="A google search results crawler",
     install_requires=['pyquery>=1.2.17', 'requests>=2.12.4', 'cchardet'],
     author='Howie Hu',
